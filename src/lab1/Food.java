3f0df1b07237a6d891568272845e893c782159fa
marina gergalova <marina.gergalova@gmail.com>
2016-09-26 22:24:27 +0300

    a second base commit

--- src/lab1/Food.java
+++ src/lab1/Food.java
@@ -3,9 +3,25 @@ package lab1;
 /**
  * Created by developer-kc3e on 26.9.16.
  */
-public class Food implements Consumable {
-    @Override
-    public void consume() {
-// TODO Auto-generated method stub
+public abstract class Food implements Consumable {
+    String name = null;
+    public Food(String name) {
+        this.name = name;
     }
+    public boolean equals(Object arg0) {
+        if (!(arg0 instanceof Food)) return false;
+// Шаг 1
+        if (name==null || ((Food)arg0).name==null) return false; // Шаг 2
+        return name.equals(((Food)arg0).name); // Шаг 3
+    }
+    public String toString() {
+        return name;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+// Реализация метода consume() удалена из базового класса
 }
